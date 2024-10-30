ab3aa435b37ab3e58a5dac61ec513c91e2c8fdd5
YigalTeperGOGIS <159950968+YigalTeperGOGIS@users.noreply.github.com>
2024-10-30 11:17:50 +0200

    Add files via upload

---
+++ template/js/config.js
@@ -0,0 +1,13 @@
+const center = { 
+   lat: 52.5159, 
+   lng: 13.3777, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   // id: 'YOUR-HERE-ID',
+   // code: 'YOUR-HERE-CODE',
+   apikey: '950M4KKA1_xJeoT1o0Dl9vMZWqrd4Hw1mw_G7hTXIzY'
+}
+
+export { center, hereCredentials };
